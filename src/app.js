53e4c387e5bd0a3cbabf35c9b50ab03750518eb1
evertonschultz <evertonf.m.schultz98@gmail.com>
2019-10-11 10:25:51 -0300

    Update for module 03

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
-import express from 'express'; // import o servidor express
-import routes from './routes'; // import as rotas de outro arquivo
+import 'dotenv/config';
 
+import express from 'express'; // import o servidor express
+import path from 'path';
+import Youch from 'youch';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
+import routes from './routes'; // import as rotas de outro arquivo
+import sentryConfig from './config/sentry';
 import './database';
 
 class App {
@@ -9,18 +16,41 @@ class App {
     // medotodo que é chamamos automaticamente quanto instanciar a classe App
     this.server = express(); // Definindo uma variavel como express
 
+    Sentry.init(sentryConfig);
+
+    this.server.use(Sentry.Handlers.requestHandler());
     this.middlewares(); // chamando os metodos
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
     // definindo metodos
     this.server.use(express.json()); // apartir de agora pode receber requisiçoes JSON
+    this.server.use(express.urlencoded({ extended: false }));
+    this.server.use(
+      '/files',
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+    );
   }
 
   routes() {
     // definindo metodos
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  // verificar
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+
+      return res.status(500).json({ error: 'Internal server error' });
+    });
   }
 }
 
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '5dff7e9abd49cc',
+    pass: '93f99fa8b8f920',
+  },
+  default: {
+    from: 'Equipe Meetapp <noreply@meetapp.com>',
+  },
+};
